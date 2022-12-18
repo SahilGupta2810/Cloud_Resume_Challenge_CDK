ed03275fa60401e19114e775d737d4f5175e189c
Sahil Gupta <guptasahil2810@gmail.com>
2022-12-18 23:20:14 +0530

    Initial commit

---
+++ test/cloud_resume_challenge_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudResumeChallengeCdk from '../lib/cloud_resume_challenge_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud_resume_challenge_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudResumeChallengeCdk.CloudResumeChallengeCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
