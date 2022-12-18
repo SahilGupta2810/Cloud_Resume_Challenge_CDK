7d1aaafb33b29a1dd9bfd4e2b90408c1490fb9d4
Sahil Gupta <guptasahil2810@gmail.com>
2022-12-19 00:12:21 +0530

    "Added S3 Deployment for uploading resume-site folder to S3"

--- lib/cloud_resume_challenge_cdk-stack.ts
+++ lib/cloud_resume_challenge_cdk-stack.ts
@@ -1,22 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import { BucketAccessControl, StorageClass } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { InlineApiDefinition } from 'aws-cdk-lib/aws-apigateway';
+// ;
+// const path = "./resume-site/index.html"
 
 export class CloudResumeChallengeCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    //Create and Configure S3 Bucket[Attach Bucket policy , Enable Website Hosting]
     
+    //Create and Configure S3 Bucket
     const bucket = new s3.Bucket(this, 'resume-storage-ap-south-1' , 
     {
       bucketName: "resume-storage-ap-south-1",
+      websiteIndexDocument: "index.html",
       publicReadAccess: true
     } );
 
+    new S3Deployment.BucketDeployment(this, "bucket-Deployment", {
+      sources: [S3Deployment.Source.asset("./resume-site")],
+      destinationBucket: bucket,
+    });
+
+    new cdk.CfnOutput(this, "BucketDomain", {
+      value: bucket.bucketWebsiteDomainName,
+    });
   }
 }
